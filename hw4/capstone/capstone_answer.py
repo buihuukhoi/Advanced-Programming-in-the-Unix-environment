a2b01459a301e43ad452457b08edae3ab0675207
buihuukhoi <khoibuihuu@gmail.com>
2020-06-13 19:03:30 +0800

    update capstone_anwser.py

--- hw4/capstone/capstone_answer.py
+++ hw4/capstone/capstone_answer.py
@@ -1,18 +1,25 @@
 import sys
 from pwn import *
 import binascii as b
+
 r = remote('aup.zoolab.org', 2530)
-for i in range(10):
-	mess = r.recvuntil('Your answer: ').decode('utf-8').split()
-	question = mess[-3]
-	question = b.a2b_hex(question)
-	context.arch = "x86_64"
-	dis_asm = disasm(question).split('\n')
-	tmp_anwser = []
-	for element in dis_asm:
-		instruction = element.split('          ')[-1].split()
-		tmp_anwser.append(' '.join(instruction))
-	anwser = '\n'.join(tmp_anwser)
-	anwser = anwser.encode('utf-8')
-	r.sendline(b.b2a_hex(anwser).decode('utf-8'))
+while(True):
+	mess = r.recvline().decode('utf-8')
+	if '>>>' in mess:
+		question = mess.split()[-1]
+		question = b.a2b_hex(question)
+		context.arch = "x86_64"
+		dis_asm = disasm(question).split('\n')
+		tmp_anwser = []
+		for element in dis_asm:
+			instruction = element.split('      ')[-1].split()
+			tmp_anwser.append(' '.join(instruction))
+		anwser = '\n'.join(tmp_anwser)
+		anwser = anwser.encode('utf-8')
+		r.sendline(b.b2a_hex(anwser).decode('utf-8'))
+		continue
+	if 'ASM' in mess:
+		print(mess)
+		break
+
 r.interactive()
